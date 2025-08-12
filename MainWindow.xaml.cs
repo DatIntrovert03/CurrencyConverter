1687873d9411d5967b3265194b3797530df6fc50
Abdul Mheta <thandolwenkosi0303@gmail.com>
2025-08-12 21:24:37 +0530

    Add buttons and improve DataGrid readability
    - Added binary image files: `delete-button.png` and `edit-button.png`.
    - Reformatted `dgvCurrency` attributes in `MainWindow.xaml` for better readability.
    - Introduced new event handlers in `MainWindow.xaml.cs`: `btnSave_Click`, `btnCancel_Click`, and `dgvCurrency_SelectedCellsChanged` (currently unimplemented).

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -88,5 +88,18 @@ namespace CurrencyConverter
             lblCurrency.Content = "";
             txtCurrency.Focus();
         }
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void dgvCurrency_SelectedCellsChanged(object sender, SelectedCellsChangedEventArgs e)
+        {
+
+        }
     }
 }
